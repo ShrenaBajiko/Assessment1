b45507e353d4bc4a1800e60d84cf96adb54080c9
ShrenaBajiko <41513194+ShrenaBajiko@users.noreply.github.com>
2025-07-17 18:06:30 +1000

    method added commit
    method added for highest, lowest and display

--- FirstAssessment/Work1.java
+++ FirstAssessment/Work1.java
@@ -14,14 +14,18 @@ public class Work1
         System.out.println("Enter the number of marks you want to input:");
         int number = input.nextInt();
         
-    //edge case for 0 or necgative number
+
+        
+    //edge case for 0 or negative number
         if (number <= 0) {
             System.out.println("Invalid number of marks. Must be greater than 0.");
             input.close();
             return;
         }
         
+        
         int [] marksList = new int[number]; 
+        
         
         for(int i =0; i<number; i++){
             int mark;
@@ -38,33 +42,47 @@ public class Work1
                 }
             }
         } 
+        
+        
         //display the entered marks in array
         System.out.println("\n Enter list of marks:");
         for(int i = 0; i<number; i++){
             System.out.println("Marks is:" + marksList[i]);
         }
         
-        //find highest and lowest marks
+        
+        int highest = findHighest(marksList);
+        int lowest = findLowest(marksList);
+        displayResults(highest, lowest);  
+        input.close();  
+    }
+    
+    public static int findHighest(int[] marksList) {
         int highest = marksList[0];
-        int lowest = marksList[0];
         for (int i=1; i< marksList.length; i++){
             if(marksList[i] > highest){
                 highest = marksList[i];
             }
             
-            if(marksList[i] < lowest){
+        }
+        return highest;
+    }
+    
+    public static int findLowest(int[] marksList) {
+        int lowest = marksList[0];
+        for (int i = 1; i < marksList.length; i++) {
+            if (marksList[i] < lowest) {
                 lowest = marksList[i];
             }
         }
-        
-    //display results
-        System.out.println("Highest Marks is" + highest);
-        System.out.println("lowest Marks is" + lowest);
-        
-        input.close();  
+        return lowest;
+
+                
     }
     
-    
-    
-    
+    public static void displayResults(int highest, int lowest) {
+        System.out.println("\nHighest Mark = " + highest);
+        System.out.println("Lowest Mark = " + lowest);
+    }
+
 }
